2fd9ec71b043730984f629b006e0b3274b7d244a
chiyoung jung <jcy0011@yahoo.com>
2025-06-09 14:40:00 +0900

    step 1: version numbering, configure_file, cxx versions etc

---
+++ Step1/build/TutorialConfig.h
@@ -0,0 +1,5 @@
+// the configured options and settings for Tutorial
+// Define Tutorial_VERSION_MAJOR and Tutorial_VERSION_MINOR
+#define Tutorial_VERSION_MAJOR 2
+#define Tutorial_VERSION_MINOR 5
+#define Tutorial_VERSION_PATCH 8
