d1dc84c1106c5a0d5e2b5a69bba12a5c30cc8195
chiyoung jung <jcy0011@yahoo.com>
2025-06-09 19:58:10 +0900

    step 2: add_library, target_link_libraries, option, etc.

--- Step2/MathFunctions/MathFunctions.cxx
+++ Step2/MathFunctions/MathFunctions.cxx
@@ -1,15 +1,25 @@
 #include "MathFunctions.h"
 
 // TODO 11: include cmath
+#ifdef USE_MYMATH
+#  include "mysqrt.h"
+#  include <iostream>
+#else
+#  include <cmath>
+#endif
 
 // TODO 10: Wrap the mysqrt include in a precompiled ifdef based on USE_MYMATH
-#include "mysqrt.h"
 
 namespace mathfunctions {
 double sqrt(double x)
 {
   // TODO 9: If USE_MYMATH is defined, use detail::mysqrt.
   // Otherwise, use std::sqrt.
+#ifdef USE_MYMATH
+  std::cout << "Mysqrt: " << x << std::endl;
   return detail::mysqrt(x);
+#else
+  return std::sqrt(x);
+#endif
 }
 }
